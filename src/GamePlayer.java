ffdd4ea2e64d274528f03cabdba8ded2ebd7cec8
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-07 19:18:27 -0400

    Added comments

--- src/GamePlayer.java
+++ src/GamePlayer.java
@@ -6,15 +6,19 @@
  *  Due Date: March 7, 2021
  */
 public class GamePlayer {
-
 	public static void main(String[] args) {
+                //creates cardHand object
 		CardHand ch = new CardHand();
+                //generate card deck
                 ch.generateHand();
-                for(Card c: ch.cards)
-                {
+                //loop through ch array of size ch.handSize note: handSize is private
+                //for-each loop through array
+                for(Card c: ch.cards) {
                     System.out.println(c.getValue() + " of " + c.getSuit());
                 }
-//               System.out.println(ch.cards); //show what happens when there is no toString()
+//               System.out.println(ch.cards); 
+//                  show what happens when there is no toString()
+//                  this will output the address of array
 	}
 
 }
