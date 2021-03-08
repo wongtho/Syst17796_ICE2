1226bac83c8fd8fa3e15b5210aa8246ab9189035
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-07 20:47:26 -0400

    Added rational to Task 4 and Task 5 in comments. Line 11 - Line 26 Task 4: one OOP principle that allows for reuse of original code Task 5: one feature that can be reused in another program.

--- src/Card.java
+++ src/Card.java
@@ -8,6 +8,22 @@
 /**
  * This class is the object of the cards
  */
+/**
+ * Task 4:
+ * The Card class and CardHand class uses Encapsulation to separate the Card object and the CardDeck (CardHand) object from
+ * each other. This improves code reusability by having the Card class model 1 single card and the CardHand class simply calls
+ * the Card class the DeckSize number of times to complete a full deck of cards.
+ *  CardHand.handSize is private and only visible within its own class
+ *  Card.Suit and Card.Value is likewise only visible within its own class
+ */
+/**
+ * Task 5:
+ * Since Card class and CardHand class is implemented separately, this package can be reused to simulate a card game between players
+ * instead of having CardHand class generate a full deck, a starting set of 5 random cards could be generated with a constructor to 
+ * simulate the starting hand of the player. A pickCard/dealCard method can be implemented in the cardHand class to simulate a player 
+ * playing a card game.
+ * The Card class is fine as is since it is a card object that creates 1 card.
+ */
 public class Card {
         //create UNO card properties
         public enum Suit {RED, YELLOW, GREEN, BLUE};
